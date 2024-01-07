28e67bf486ee489ec23e37caa846ac637068fff0
canumut05 <hasozdemirumutcan@gmail.com>
2024-01-07 16:22:57 +0300

    Proje dosyası ekle.

---
+++ odev+saat/Program.cs
@@ -0,0 +1,16 @@
+﻿int saat, dakika, saniye, gün, beklenensaat = 1;
+
+    for (saat = 0; saat < 24; saat++)
+    {
+        for (dakika = 0; dakika < 60; dakika++)
+        {
+            for (saniye = 0; saniye < 60; saniye++)
+            {
+                Console.WriteLine("dijital saat"+ "= "  + saat + ":" + dakika+ ":"+ saniye);
+                
+                Thread.Sleep(beklenensaat*500);
+                Console.Clear();
+            }
+        }
+    }
+
